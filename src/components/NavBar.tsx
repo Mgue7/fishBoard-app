ac12df65b5f8eb41fc4c08f49c006a0d28445823
Mgue7 <matgue8@gmail.com>
2025-08-02 14:19:33 -0500

    Update Commit1

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,17 +4,6 @@ function NavBar() {
       <nav className="navbar bg-navbar">
         <div className="container-fluid">
           <a className="navbar-brand brand-text">FISHBOARD</a>
-          {/* <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-search" type="submit">
-              Search
-            </button>
-          </form> */}
         </div>
       </nav>
     </>
